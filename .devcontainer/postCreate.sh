bd6f194cc1b4902cfb74fd03f12627dc8b51f12f
Dion Hutchings <dionhut@outlook.com>
2023-11-20 11:26:14 -0800

    Update postCreate.sh
    Fixed build path

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,7 @@ sudo chown vscode node_modules
 sudo chown vscode build
 
 git config --global --add safe.directory /workspaces/bun
-echo export PATH=\$PATH:build >> ~/.bashrc
+echo export PATH=\$PATH:/workspaces/bun/build >> ~/.bashrc
 
 echo "🎉 Bun Development Environment devcontainer setup complete! 🎉"
 echo "To build bun, run 'bun setup'"
